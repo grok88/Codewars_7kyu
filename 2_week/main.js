0c2ad89d37a11f7d986d918649c1fb4d972959af
Alex <grok88@tut.by>
2020-05-08 22:14:14 +0300

    add next 2 tasks

--- 2_week/main.js
+++ 2_week/main.js
@@ -151,3 +151,63 @@ function friend(friends){
 }
 
 console.log(friend(["Ryag", "Jimmy", "123", "4", "Cool Man"]));
+
+
+
+
+// -------------------DAY 4
+
+
+// TASK 1
+// Number of People in the Bus
+
+// Number of people in the bus
+// There is a bus moving in the city, and it takes and drop some people in each bus stop.
+// You are provided with a list (or array) of integer arrays (or tuples). Each integer array has two items which represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+// Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+// Take a look on the test cases.
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+// The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+
+//my
+var number = function(busStops){
+	let getInto = 0;
+	let getOff = 0;
+	busStops.forEach(([first,second]) => {
+		getInto +=first;
+		getOff +=second;
+	});
+	return getInto - getOff;
+}
+
+// The best
+// const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
+
+console.log(number([[10,0],[3,5],[5,8]]));
+
+//TASK 2
+// Find the divisors!
+
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+
+function divisors(integer) {
+	let arr = [];
+
+	// let flag =1;
+	// for(let i = 2; i < Math.sqrt(integer); i++){
+	// 	if (integer % i == 0)  flag = 0;
+	// }
+
+	for(let i = 2; i < integer; i++){
+			if(integer % i === 0) arr.push(i);
+	}
+	return arr.length ?  arrcd /D : `${integer} is prime`;
+	
+};
+
+console.log(divisors(15));
