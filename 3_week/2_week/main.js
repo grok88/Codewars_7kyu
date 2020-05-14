82d865c03a4d286d0b79f983fe9b64f2560c143a
Alex <grok88@tut.by>
2020-05-14 09:24:41 +0300

    Add next 2 tasks

--- 3_week/2_week/main.js
+++ 3_week/2_week/main.js
@@ -94,3 +94,70 @@ function reverseWords(str) {
 }
 
 console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
+
+
+//----DAY 4-------------------------
+
+
+//TASK 1
+// Money, Money, Money
+
+// Mr. Scrooge has a sum of money 'P' that wants to invest, and he wants to know how many years 'Y' this sum has to be kept in the bank in order for this sum of money to amount to 'D'.
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly, and the new sum is re-invested yearly after paying tax 'T' on the interests that were just gained (and only on the interests part).
+// Example:
+//   Let P be the Principal = 1000.00      
+//   Let I be the Interest Rate = 0.05      
+//   Let T be the Tax Rate = 0.18      
+//   Let D be the Desired Sum = 1100.00
+
+// After 1st Year -->
+//   P = 1041.00
+// After 2nd Year -->
+//   P = 1083.86
+// After 3rd Year -->
+//   P = 1128.30
+// Thus Mr. Scrooge has to wait for 3 years for the initial pricipal to ammount to the desired sum.
+
+// Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
+
+// Assumptions : Assume that Desired Principal 'D' is always greater than the initial principal, however it is best to take into consideration that if the Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
+
+
+function calculateYears(principal, interest, tax, desired) {
+	// your code
+	if (principal === desired) return 0;
+	let initial = principal;
+	let year = 0;
+	while (desired > initial ){
+		let interestYear = initial *  interest; 
+		let taxSum = interestYear * tax;
+		let amount = interestYear - taxSum;
+		initial += amount;
+		year++;
+	}
+
+	return year;
+}
+
+console.log(calculateYears(1000, 0.05, 0.18, 1100));
+
+
+// Task2:
+//Odd or Even?
+
+// Given a list of numbers, determine whether the sum of its elements is odd or even.
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Example:
+// odd_or_even([0])          ==  "even"
+// odd_or_even([0, 1, 4])    ==  "odd"
+// odd_or_even([0, -1, -5])  ==  "even"
+
+function oddOrEven(array) {
+	if(array.length === 0 ) return 'even'
+	
+	return [...array].reduce((initial, item) => initial + item) %2 === 0 ? 'even' : 'odd';
+}
+
+console.log(oddOrEven([0, 1, 5]));
