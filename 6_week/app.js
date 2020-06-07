3bfef2269efd9e6eb97174643c27b1d9b245ee49
Alex <grok88@tut.by>
2020-06-07 22:24:38 +0300

    Add 1 kata - 7(Find the capitals)

--- 6_week/app.js
+++ 6_week/app.js
@@ -148,3 +148,51 @@ function roundToNext5(n){
 
 console.log(roundToNext5(-3));
 
+
+//-------------DAY 4----------------
+
+//Task1
+
+//Find the capitals
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+var capitals = function (word) {
+	let arr = [];
+	word.split('').forEach((elem, i) => {
+		if(/[A-Z]/.test(elem)){
+			arr.push(i) ;
+		}
+	});
+	return arr;
+}
+console.log(capitals('CodEWaRs'));
+
+
+//Task2
+// Palindrome chain length
+
+// Number is a palindrome if it is equal to the number with digits in reversed order. For example, 5, 44, 171, 4884 are palindromes and 43, 194, 4773 are not palindromes.
+// Write a method palindrome_chain_length which takes a positive number and returns the number of special steps needed to obtain a palindrome. The special step is: "reverse the digits, and add to the original number". If the resulting number is not a palindrome, repeat the procedure with the sum until the resulting number is a palindrome.
+// If the input number is already a palindrome, the number of steps is 0.
+// Input will always be a positive integer.
+// For example, start with 87:
+// 87 + 78 = 165; 165 + 561 = 726; 726 + 627 = 1353; 1353 + 3531 = 4884
+// 4884 is a palindrome and we needed 4 steps to obtain it, so palindrome_chain_length(87) == 4
+
+
+var palindromeChainLength = function(n) {
+	const temp = String(n);
+	let reverse = temp.split('').reverse().join('');
+	console.log(temp);
+	if ( reverse === String(n) ){
+		return 0;
+	}
+
+	return n;
+};
+
+console.log(palindromeChainLength(87));
