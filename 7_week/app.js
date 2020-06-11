a715d8559414a3bb27e53f342d74f8e484a7def0
Alex <grok88@tut.by>
2020-06-11 08:58:25 +0300

    add next 1 kata -7(Sum of numbers from 0 to N)

--- 7_week/app.js
+++ 7_week/app.js
@@ -46,3 +46,42 @@ function addLetters(...letters) {
 }
 
 console.log(addLetters(["s", "k", "j", "f", "b", "k"]));
+
+//DAy 2
+
+//TASK1
+// Sum of numbers from 0 to N
+// > 6 Output: 0+1+2+3+4+5+6 = 21
+// Input: > -15 Output: -15<0
+// Input: > 0 Output: 0=0
+
+
+//My stupid solution
+
+var SequenceSum = (function() {
+	function SequenceSum() {}
+	SequenceSum.showSequence = function(count) {
+		let arr = [];
+		let str = '';
+		if(count < 0){
+			return `${count}<0`;
+		} else if(count === 0){
+			return `${count}=0`
+		} else {
+			for (let i = 0; i<= count; i++){
+				arr.push(i);
+				if(i < count){
+					str += i + '+';
+				} else {
+					str += i;
+				}
+				
+			}
+			let sum =  arr.reduce((acc,elem) => acc + elem);
+
+			return str + ' ' + '=' + ' ' + sum
+		}
+	};
+	return SequenceSum;
+})();
+console.log(SequenceSum.showSequence(6));
