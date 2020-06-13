3cc8f2b8bcd166f09284218a0deed481fbf4406d
Alex <grok88@tut.by>
2020-06-13 20:01:34 +0300

    Easy kata

--- 7_week/app.js
+++ 7_week/app.js
@@ -113,3 +113,17 @@ function capitalize(s){
 };
 
 console.log(capitalize("abcdef"));
+
+
+class SmallestIntegerFinder {
+	findSmallestInt(args) {
+	  return Math.min(... args);
+	}
+}
+
+// Find the smallest integer in the array
+// Given an array of integers your solution should find the smallest integer.
+// For example:
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+
