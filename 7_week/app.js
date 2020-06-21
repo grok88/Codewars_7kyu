bb081c5f464bcd5ee36279e77c1a3c67682a4957
Alex <grok88@tut.by>
2020-06-21 08:46:32 +0300

    Add next 2 kata 7(Sum of Minimums!,Form The Largest )

--- 7_week/app.js
+++ 7_week/app.js
@@ -167,6 +167,42 @@ var isAnagram = function(test, original) {
 
 console.log(isAnagram("foefet", "toffee"));
 
+//-----------DAY N-----------
+
+//TASk1
+//Sum of Minimums!
+// Given a 2D array of size m * n. Your task is to find the sum of minimum value in each row.
+// For Example:
+// [
+//   [1, 2, 3, 4, 5],       // minimum value of row is 1
+//   [5, 6, 7, 8, 9],       // minimum value of row is 5
+//   [20, 21, 34, 56, 100]  // minimum value of row is 20
+// ]
+// So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+// Note: You will be always given non-empty array containing Positive values.
+
+function sumOfMinimums(arr) {
+	return  arr.map(elem => {
+		return Math.min(...elem);
+	}).reduce((acc,item) => {
+return acc+item;
+	},0);
+}
+
+
+console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]));
+
+
+//Task2
+//Form The Largest
+// Given a number , Return _The Maximum number _ could be formed from the digits of the number given .
+
+function maxNumber(n){
+	return +String(n).split('').sort((a,b) => b-a).join('');
+}
+
+console.log(maxNumber(63792));
+
 
 
 
