5a4074e096206fa27a47232d9612702a9ac47463
Alex <grok88@tut.by>
2020-06-22 08:53:19 +0300

    6kata - (Multiples of 3 or 5)

---
+++ 6kuo/1_week/app.js
@@ -0,0 +1,24 @@
+//-----------------------DAY 1-------------------------
+
+//task1
+// Multiples of 3 or 5
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number){
+	if(number <=0) return 0 ;
+	let arr =[];
+	for(let i = 0; i < number; i++){
+		if(i%3 === 0 || i%5 ===0){
+				arr.push(i);
+		
+		}
+	}
+	return arr.reduce((acc,num) => {
+		return acc+num;
+	})
+}
+
+console.log(solution(10));
