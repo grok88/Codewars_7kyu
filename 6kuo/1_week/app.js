88922ae7cfcd94f6a896f55602b95e4fd926eb56
Alex <grok88@tut.by>
2020-06-23 09:06:42 +0300

    Add 6(Sum of Digits / Digital Root)

--- 6kuo/1_week/app.js
+++ 6kuo/1_week/app.js
@@ -22,3 +22,25 @@ function solution(number){
 }
 
 console.log(solution(10));
+
+
+//-----------------------DAY 2-------------------------
+
+// Sum of Digits / Digital Root
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(n) {
+	let arrTemp = String(n);
+	while(arrTemp.length !== 1){
+		arrTemp = arrTemp.split('');
+		arrTemp = String(arrTemp.reduce((acc,num) => acc+ Number(num),0));
+	}
+	return +arrTemp;
+}
+console.log(digital_root(132189));
