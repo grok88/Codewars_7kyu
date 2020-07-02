1ed31c1e59dd8211014139a98315b16ff3750d4e
Alex <grok88@tut.by>
2020-07-02 10:12:11 +0300

    add next kata -7(Persistent Bugger.)

--- 6kuo/1_week/app.js
+++ 6kuo/1_week/app.js
@@ -44,3 +44,32 @@ function digital_root(n) {
 	return +arrTemp;
 }
 console.log(digital_root(132189));
+
+
+//Persistent Bugger.
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+// For example:
+
+//  persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+//                        // and 4 has only one digit
+//  persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+//                         // 1*2*6 = 12, and finally 1*2 = 2
+//  persistence(4) === 0 // because 4 is already a one-digit number
+
+function persistence(num) {
+	let count = 0;
+	if(String(num).length === 1 ){
+		return 0
+	}
+	let arr = String(num).split('');
+	
+	while(arr.length !==1){
+		count++;
+		let res = arr.reduce((acc, num) => {
+			return acc * num
+		});
+		arr = String(res).split('');
+	}
+	return count;
+}
+console.log(persistence(25));
