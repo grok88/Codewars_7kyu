a1909757a4a6cbf67ffc1f3092985bddb25fd212
Alex <grok88@tut.by>
2020-07-04 08:49:44 +0300

    add next 1 kata 6(Find The Parity Outlier)

--- 6kuo/1_week/app.js
+++ 6kuo/1_week/app.js
@@ -73,3 +73,24 @@ function persistence(num) {
 	return count;
 }
 console.log(persistence(25));
+
+
+// Find The Parity Outlier
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+
+function findOutlier(integers){
+	let status = integers.filter(num => num%2 ===0);
+	if (status.length === 1) {
+		return status[0]
+	} else {
+		return integers.filter(num => num%2 !==0)[0]
+	}
+}
+
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
