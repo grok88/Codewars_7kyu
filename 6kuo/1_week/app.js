d960db299b6160fb6207a0f11ddf32b4eabe4929
Alex <grok88@tut.by>
2020-07-21 08:54:27 +0300

    kata - 6(Bit Counting)

--- 6kuo/1_week/app.js
+++ 6kuo/1_week/app.js
@@ -227,3 +227,23 @@ function duplicateEncode(word){
 	.join('');
 }
 console.log(duplicateEncode("))))u))O)cly)a)"));
+
+
+// ..---------------------NEXT KATA
+
+// Bit Counting
+
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+var countBits = function(n) {
+	let binary = n.toString(2);
+	let count = 0;
+	binary.split('').forEach(num => {
+		if(num=== '1') count++;
+	})
+	return count;
+};
+
+console.log(countBits(1234));
