84ba45abcdb85d8925847e3507009ca9c6e53657
Alex <grok88@tut.by>
2020-07-22 09:55:06 +0300

    next kata - 6( Your order, please)

--- 6kuo/1_week/app.js
+++ 6kuo/1_week/app.js
@@ -247,3 +247,57 @@ var countBits = function(n) {
 };
 
 console.log(countBits(1234));
+
+// ..---------------------------------
+
+// Your order, please
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+
+// my stupid
+function order(words){
+	if(words.length === 0){
+		return ''
+	}
+	let arr = words.split(' ');
+	let temp = [];
+	let res = [];
+	arr.map(elem => {
+		console.log(elem.split(''));
+		return elem.split('').forEach(l => {
+			if(parseInt(l)){
+				temp.push(l)
+			}
+		});
+	})
+	temp.sort((a,b) =>  a - b)
+	temp.forEach(item => {
+		arr.forEach(w => {
+			if(w.indexOf(item) !== -1){
+				console.log(w);
+				res.push(w)
+			}
+		})
+	})
+	console.log(res);
+	console.log(temp);
+	return res.join(' ');
+	// ...
+}
+
+// the BEST
+// function order(words){
+  
+// 	return words.split(' ').sort(function(a, b){
+// 		return a.match(/\d/) - b.match(/\d/);
+// 	 }).join(' ');
+//   }   
+ 
+console.log(order("is2 Thi1s T4est 3a"));
