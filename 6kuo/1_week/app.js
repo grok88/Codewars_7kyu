dc45951d83981556e191b2dd4cec4537cee6a585
Alex <grok88@tut.by>
2020-07-27 10:12:04 +0300

    next 1kata - 6(Vasya - Clerk)

--- 6kuo/1_week/app.js
+++ 6kuo/1_week/app.js
@@ -430,4 +430,42 @@ function iqTest(numbers){
 return 	odd.length < even.length ? arr.indexOf(odd[0]) + 1 : arr.indexOf(even[0]) + 1
 }
 
-console.log(iqTest("2 4 7 8 10"));
+console.log(iqTest("2 4 7 8 10"));	
+
+
+//--------------------------- 
+
+//Vasya - Clerk
+// The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+// Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+// Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
+// Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
+
+// Examples:
+// tickets([25, 25, 50]) // => YES 
+// tickets([25, 100]) // => NO. Vasya will not have enough money to give change to 100 dollars
+// tickets([25, 25, 50, 50, 100]) // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
+
+function tickets(peopleInLine){
+	let  change25 =0 ,
+	change50 = 0;
+	for(let i =0; i< peopleInLine.length; i++){
+		if(peopleInLine[i] === 25){
+			change25++;
+		}else if(peopleInLine[i] === 50){
+			change25--;
+			change50++;
+		}else if(peopleInLine[i] === 100 && change50>0){
+			change50--;
+			change25--;
+		} else if(peopleInLine[i] === 100 && change50===0){
+			change25-=3;
+		}
+		if(change25 < 0 || change50<0){
+			return 'NO'
+		};
+	}
+	return 'YES'
+}
+
+console.log(tickets([ 25, 50, 25, 50, 100, 25, 25, 50 ]));
