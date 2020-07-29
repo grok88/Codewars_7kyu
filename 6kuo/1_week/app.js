667cd56f43648eee0a0371cd6612a09537fa574d
Alex <grok88@tut.by>
2020-07-29 10:05:08 +0300

    next 1 kata 6(Valid Braces)

--- 6kuo/1_week/app.js
+++ 6kuo/1_week/app.js
@@ -485,3 +485,39 @@ function longestConsec(strarr, k) {
 	return test[length.indexOf(Math.max(...length))].join('') ;
 }
 console.log(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"],2))
+
+
+//-----------------------NEXT -------------------
+
+//Valid Braces
+// Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+// This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+// All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+// What is considered Valid?
+// A string of braces is considered valid if all braces are matched with the correct brace.
+// Examples
+// "(){}[]"   =>  True
+// "([{}])"   =>  True
+// "(}"       =>  False
+// "[(])"     =>  False
+// "[({})](]" =>  False
+
+function validBraces(braces){
+	let arr = braces.split('');
+	let res = [];
+	arr.forEach(el => {
+		if(el ==='(' || el ==='{' | el ==='['){
+			res.push(el);
+		}else {
+			let lastValue = res[res.length-1];
+			if( (el === ']' && lastValue === '[') || (el === '}' && lastValue === '{') || (el === ')' && lastValue === '(')){
+        res.pop()
+      	} else {
+			res.length = 1111;
+		  }
+		}
+	})
+	return res.length === 0
+}
+
+console.log(validBraces( "(})" ));
