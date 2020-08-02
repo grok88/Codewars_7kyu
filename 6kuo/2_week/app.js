5f17c78efa9bccab7d633fe9acc65221d0050060
Alex <grok88@tut.by>
2020-08-02 11:19:15 +0300

    add next cata 7(Gauß needs help! (Sums of a lot of numbers).)

--- 6kuo/2_week/app.js
+++ 6kuo/2_week/app.js
@@ -91,3 +91,23 @@ function findMissingLetter(array){
 // console.log('a'.charCodeAt());
 
 console.log(findMissingLetter(['a','b','c','d','f']));
+
+//-----------------Day3------------------
+
+// Gauß needs help! (Sums of a lot of numbers).
+
+// Due to another of his misbehaved, the primary school's teacher of the young Gauß, Herr J.G. Büttner, to keep the bored and unruly young schoolboy Karl Friedrich Gauss busy for a good long time, while he teaching arithmetic to his mates, assigned him the problem of adding up all the whole numbers from 1 through a given number n.
+// Your task is to help the young Carl Friedrich to solve this problem as quickly as you can; so, he can astonish his teacher and rescue his recreation interval.
+// Here's, an example:
+// f(n=100) // returns 5050 
+
+
+function f(n){
+	let res =0;
+	for(let i =1; i <= n; i++){
+		res += i;
+	}
+	return Number.isInteger(n)&& n > 0 ? res : false;
+};
+
+console.log(f(100));
