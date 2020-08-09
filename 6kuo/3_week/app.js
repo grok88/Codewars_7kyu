4122ee0e3a09e7ccc8b78a7e51fa900ec4194ae8
Alex <grok88@tut.by>
2020-08-09 08:45:00 +0300

    next kata 6(Count the smiley faces!)

--- 6kuo/3_week/app.js
+++ 6kuo/3_week/app.js
@@ -28,4 +28,37 @@ function vowelIndices(word){
 	return rez;
 }
 
-console.log(vowelIndices("supercalifragilisticexpialidocious"));
+// console.log(vowelIndices("supercalifragilisticexpialidocious"));
+
+
+// ..----DAY2 ------------------------
+
+
+// Count the smiley faces!
+
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+// Rules for a smiling face:
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+function countSmileys(arr) {
+	if(arr.length === 0 ) return 0;
+	let rez = [];
+	arr.forEach(el => {
+		if(el.match(/^[: ;][- ~]?[) D]$/)){
+			rez.push(el.match(/^[: ;][- ~]?[) D]$/)[0])
+		}
+	})
+	return rez.length;
+}
+
+console.log(countSmileys([':D',':~)',';~D',':)']));
