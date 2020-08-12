9724d40546d57f9ff4c6ecc332dcba730e5c15c1
Alex <grok88@tut.by>
2020-08-12 08:58:09 +0300

    kata 6(Does my number look big in this?)

--- 6kuo/3_week/app.js
+++ 6kuo/3_week/app.js
@@ -62,3 +62,27 @@ function countSmileys(arr) {
 }
 
 console.log(countSmileys([':D',':~)',';~D',':)']));
+
+
+// ..-----------------------------
+// Does my number look big in this?
+// ..A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+// For example, take 153 (3 digits):
+
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1634 (4 digits):
+
+//     1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+
+function narcissistic(value) {
+	// Code me to return true or false
+	let str = String(value);
+	let pow = str.length;
+	let res = str.split('').map(( el) => {
+		return el**pow;
+	}).reduce((acc,el) => acc + el);
+	return value === res;
+}
+
+console.log(narcissistic( 371 ));
